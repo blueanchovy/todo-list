8e2204ea82d6814baa8ac6fa9117315cf3f97d71
Manish Jha <manishjhazyx@gmail.com>
2022-03-21 17:37:12 +0530

    created login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Login.css";
 
 function Login() {
+  // const signIn = (e) => {
+  //     e.preventDefault();
+
+  //     signInWithPopup(auth, provider).catch((error) => {
+  //       alert(error.message);
+  //     });
+  //   };
+
   return (
     <div className="login">
-      <div className="login__window">
-        <h2>Welcome to my To-do List</h2>
+      <div className="login__inner">
+        <h1>Sign in to Manish's Todo List</h1>
+
+        <button>Sign in with Google</button>
       </div>
     </div>
   );
